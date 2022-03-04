ff098d898a06b730f6710ddb28a80773a28110aa
blueneyes1 <blueneyes@nate.com>
2022-03-04 11:31:58 +0900

    DB 수정

--- src/main/webapp/WEB-INF/db.sql
+++ src/main/webapp/WEB-INF/db.sql
@@ -5,7 +5,7 @@
 
 drop table genies_member;
 create table genies_member(
-    member_idx       number(4) primary key,
+    member_idx       number(50) primary key,
     member_id      varchar2(20),
     member_pw     varchar2(20),
     member_name   varchar2(20),
@@ -37,7 +37,7 @@ select * from genies_member;
 -- 공지사항   
 drop table genies_notice;
 create table genies_notice(
-    notice_idx      number(4) primary key,
+    notice_idx      number(50) primary key,
     notice_title    varchar2(100),
     notice_content  varchar2(4000),
     notice_date     date default sysdate
@@ -49,11 +49,13 @@ create sequence genies_notice_seq;
 -- 장바구니
 drop table genies_basket;
 create table genies_basket(
-    basket_idx      number(4) primary key,
+    basket_idx      number(50) primary key,
     basket_member_id    varchar2(20),
     basket_product_idx	  number(4),
     basket_count     number(2),
-    basket_price     number(8)
+    basket_price     number(8),
+    foreign key (basket_member_id)references genies_member(member_id)
+    foreign key (basket_product_idx)references genies_product(product_idx)
 );
 
 drop sequence genies_basket_seq;
@@ -62,7 +64,7 @@ create sequence genies_basket_seq;
 -- FAQ
 drop table genies_faq;
 create table genies_faq(
-    faq_idx      number(4) primary key,
+    faq_idx      number(50) primary key,
     faq_title    varchar2(100),
     faq_content  varchar2(2000)
 );
@@ -73,7 +75,7 @@ create sequence genies_faq_seq;
 -- 1:1 문의 답변
 drop table genies_one2one_reply;
 create table genies_one2one_reply(
-    one2one_reply_idx      number(4) primary key,
+    one2one_reply_idx      number(50) primary key,
     one2one_reply_member_id    varchar2(20),
     one2one_reply_member_name  varchar2(20),
     one2one_reply_title			varchar2(100),
@@ -88,7 +90,7 @@ create sequence genies_one2one_reply_seq;
 -- 1:1문의
 drop table genies_one2one;
 create table genies_one2one(
-    one2one_idx      number(4) primary key,
+    one2one_idx      number(50) primary key,
     one2one_member_id     varchar2(20),
     one2one_member_name    varchar2(20),
     one2one_title     varchar2(100),
@@ -102,7 +104,7 @@ create sequence genies_one2one_seq;
 -- 결제정보
 drop table genies_pay;
 create table genies_pay(
-    pay_idx      number(4) primary key,
+    pay_idx      number(50) primary key,
     pay_member_id     varchar2(20),
     pay_receiver       varchar2(20),		-- 받는사람
     pay_phone  		varchar2(13),
@@ -113,16 +115,32 @@ create table genies_pay(
     pay_cost		number(4) default '3000',		-- 배송비
     pay_total		number(8),
     pay_delivery	number(1) default '1',		-- 0:결제취소 1:배송준비 2:배송중 3:배송완료
-    pay_date		date default sysdate
+    pay_date		date default sysdate,
+    foreign key (pay_member_id)references genies_member(member_id)
 );
 
 drop sequence genies_pay_seq;
 create sequence genies_pay_seq;
 
+-- 주문제품
+drop table genies_order;
+create table genies_order(
+	order_idx		number(50) primary key,
+	order_pay_idx		number(50),
+	order_product_idx		number(50),
+	order_count			number(2),
+	order_price			number(8),
+	foreign key (order_pay_idx)references genies_pay(pay_idx),
+	foreign key (order_product_idx)references genies_product(product_idx)
+);
+
+drop sequence genies_order_seq;
+create sequence genies_order_seq;
+
 -- 제품
 drop table genies_product;
 create table genies_product(
-    product_idx      number(4) primary key,
+    product_idx      number(50) primary key,
     product_type		varchar2(20),
     product_name     varchar2(50),
     product_brand       varchar2(50),
@@ -141,13 +159,16 @@ create sequence genies_product_seq;
 -- 제품리뷰
 drop table genies_product_review;
 create table genies_product_review(
-    review_idx      number(4) primary key,
-    review_product_idx    number(4),
+    review_idx      number(50) primary key,
+    review_order_idx		number(50),
+    review_product_idx    number(50),
     review_member_id  		varchar2(20),
     review_title			varchar2(50),
     review_content		varchar2(1000),
     review_grade			char(1),
-    review_date		date default sysdate
+    review_date		date default sysdate,
+    foreign key (review_member_id) references genies_member(member_id),
+    foreign key (review_order_idx) references genies_order(order_idx)
 );
 
 drop sequence genies_product_review_seq;
@@ -155,10 +176,10 @@ create sequence genies_product_review_seq;
 
 drop table genies_review_reply;
 create table genies_review_reply(
-    review_reply_idx       number(4) primary key,
+    review_reply_idx       number(50) primary key,
     review_reply_content   varchar2(1000),
     review_reply_date      date default sysdate,
-    review_reply_review_idx 		number(4)
+    review_reply_review_idx 		number(50)
 );
 
 drop sequence genies_review_reply_seq;
