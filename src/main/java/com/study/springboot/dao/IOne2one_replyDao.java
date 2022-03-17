a805efce00edd3298f69d040671a7c2aa34643dd
Prayetihw <prayetihw@gmail.com>
2022-03-17 22:37:37 +0900

    one2one

--- src/main/java/com/study/springboot/dao/IOne2one_replyDao.java
+++ src/main/java/com/study/springboot/dao/IOne2one_replyDao.java
@@ -1,8 +1,15 @@
 package com.study.springboot.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.study.springboot.dto.One2one_replyDto;
+
 
 @Mapper
 public interface IOne2one_replyDao {
-
+		public List<One2one_replyDto> reply_list( String one2one_reply_member_id );
+		public int reply_write(String one2one_reply_title, String one2one_reply_content, String one2one_reply_member_id);
+		public int reply_deleteDto(String one2one_reply_idx);
 }
