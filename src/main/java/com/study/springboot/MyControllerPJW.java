cb3412267c406c9526198745baf2cc61be44fa73
Prayetihw <prayetihw@gmail.com>
2022-03-17 22:38:08 +0900

    Revert "one2one"
    This reverts commit a805efce00edd3298f69d040671a7c2aa34643dd.

--- src/main/java/com/study/springboot/MyControllerPJW.java
+++ src/main/java/com/study/springboot/MyControllerPJW.java
@@ -11,164 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.study.springboot.dao.IOne2oneDao;
-import com.study.springboot.dto.One2oneDto;
-import com.study.springboot.dao.IOne2one_replyDao;
-import com.study.springboot.dto.One2one_replyDto;
+import com.study.springboot.dao.IBasketDao;
+import com.study.springboot.dto.BasketDto;
+import com.study.springboot.dto.MemberDto;
+import com.study.springboot.dto.PayDto;
+import com.study.springboot.dto.ProductDto;
+import com.study.springboot.dto.OrderDto;
 
-import com.study.springboot.service.One2oneService;
-import com.study.springboot.service.One2one_replyService;
-
+import com.study.springboot.service.BasketService;
+import com.study.springboot.service.MemberService;
+import com.study.springboot.service.payService;
+import com.study.springboot.service.ProductService;
+import com.study.springboot.service.OrderService;
 
 @Controller
 public class MyControllerPJW {
 
-	@Autowired
-	IOne2oneDao one2onedao;
-	@Autowired
-	IOne2one_replyDao replydao;
-
-
-	@RequestMapping("/customer/customer01")
-	public String listForm( Model model,
-			HttpServletRequest request ) {
-		
-		List<One2oneDto> list = one2onedao.list();
-		model.addAttribute("mainPage", "one2one/list.jsp");
-		
-		//System.out.println( list );
-		
-		return "one2one/listForm";  //"listForm.jsp" 디스패치함.
-	}
-	
-	
-	@RequestMapping("/customer/writeForm")
-	public String writeForm(HttpServletRequest request ) {
-		
-		return "one2one/writeForm"; //"writeForm.jsp" 디스패치함.
-	}
-	
-	@RequestMapping("/customer/writeAction")
-	@ResponseBody
-	public String writeAction( @RequestParam("one2one_member_id") String one2one_member_id,
-								@RequestParam("one2one_title") String one2one_title,
-								@RequestParam("one2one_content") String one2one_content,
-								HttpServletRequest request) 
-	{
-		int result = one2onedao.write(one2one_member_id, one2one_title, one2one_content);
-		if( result == 1 ) {
-			System.out.println("글쓰기 성공!");
-			
-			//return "redirect:listForm"; //listForm.jsp 으로 리다이렉트 됨.
-			return "<script>alert('글쓰기 성공!'); location.href='/customer/customer01';</script>";
-		}else {
-			System.out.println("글쓰기 실패!");
-			
-			//return "redirect:writeForm"; //writeForm.jsp 으로 리다이렉트 됨.
-			return "<script>alert('글쓰기 실패!'); location.href='/customer/writeForm';</script>";
-		}
-		
-	}
-	
-	
-	@RequestMapping("/customer/listForm")
-	public String contentForm(@RequestParam("one2one_idx") String one2one_idx,
-			                   Model model,
-			       			   HttpServletRequest request ) {
-		
-		
-		//게시글 보기
-		One2oneDto dto = one2onedao.viewDto( one2one_idx );
-		model.addAttribute("dto", dto);
-		
-		//댓글 리스트 가져오기
-		List<One2one_replyDto> reply_list = replydao.reply_list( one2one_idx  );
-		model.addAttribute("reply_list", reply_list);
-		
-		return "one2one/contentForm"; //contentForm.jsp 으로 리다이렉트 됨.
-	}
-	
-	@RequestMapping("/updateAction")
-	@ResponseBody
-	public String updateAction( @RequestParam("one2one_idx") String one2one_idx,
-								@RequestParam("one2one_member_id") String one2one_member_id,
-								@RequestParam("one2one_title") String one2one_title,
-								@RequestParam("one2one_content") String one2one_content,
-								HttpServletRequest request ) {
-		
-		int result = one2onedao.updateDto(one2one_idx, one2one_member_id, one2one_title, one2one_content);
-		if( result == 1 ) {
-			System.out.println("글수정 성공!");
-			
-			//return "redirect:listForm"; //listForm.jsp 으로 리다이렉트 됨.
-			return "<script>alert('글수정 성공!'); location.href='/one2one/listForm';</script>";
-		}else {
-			System.out.println("글수정 실패!");
-			
-			//return "redirect:contentForm?one2one_idx=" + one2one_idx; //updateForm 으로 리다이렉트 됨.
-			return "<script>alert('글수정 실패!'); location.href='/one2one/contentForm?one2one_idx=" + one2one_idx + "';</script>";
-		}
-		
-	}
-	
-	@RequestMapping("/deleteAction")
-	@ResponseBody
-	public String deleteAction(@RequestParam("one2one_idx") String one2one_idx,
-								HttpServletRequest request)
-	{
-		int result = one2onedao.deleteDto(one2one_idx);
-		if( result == 1 ) {
-			System.out.println("글삭제 성공!");
-			
-			//return "redirect:listForm"; //listForm.jsp 으로 리다이렉트 됨.
-			return "<script>alert('글삭제 성공!'); location.href='/listForm';</script>";
-		}else {
-			System.out.println("글삭제 실패!");
-			
-			//return "redirect:contentForm?one2one_idx=" + one2one_idx; //updateForm 으로 리다이렉트 됨.
-			return "<script>alert('글수정 실패!'); location.href='/contentForm?one2one_idx=" + one2one_idx + "';</script>";
-		}
-		
-	}
-	
-	@RequestMapping("/writeReplyAction")
-	@ResponseBody
-	public String writeReplyAction( @RequestParam("one2one_reply_content") String one2one_reply_content,
-								@RequestParam("one2one_reply_title") String one2one_reply_title,
-								@RequestParam("one2one_reply_member_id") String one2one_reply_member_id,
-								HttpServletRequest request) 
-	{
-		
-		int result = replydao.reply_write(one2one_reply_title, one2one_reply_content, one2one_reply_member_id);
-		if( result == 1 ) {
-			System.out.println("답변달기 성공!");
-			
-			return "<script>alert('답변달기 성공!'); location.href='/contentForm?one2one_idx=" + one2one_reply_member_id + "';</script>";
-		}else {
-			System.out.println("답변달기 실패!");
-			
-			return "<script>alert('답변달기 실패!'); location.href='/contentForm?one2one_idx=" + one2one_reply_member_id + "';</script>";
-		}
-		
-	}
-	
-	@RequestMapping("/deleteReplyAction")
-	@ResponseBody
-	public String deleteReplyAction(@RequestParam("one2one_reply_idx") String one2one_reply_idx,
-								   @RequestParam("one2one_idx") String one2one_idx,
-								HttpServletRequest request)
-	{
-		int result = replydao.reply_deleteDto(one2one_reply_idx);
-		if( result == 1 ) {
-			System.out.println("답변삭제 성공!");
-			
-			return "<script>alert('답변삭제 성공!'); location.href='/contentForm?one2one_idx=" + one2one_idx + "';</script>";
-		}else {
-			System.out.println("답변삭제 실패!");
-			
-			return "<script>alert('답변삭제 실패!'); location.href='/contentForm?one2one_idx=" + one2one_idx + "';</script>";
-		}
-		
-	}
 	
 }
--- src/main/java/com/study/springboot/dao/IOne2oneDao.java
+++ src/main/java/com/study/springboot/dao/IOne2oneDao.java
@@ -1,18 +1,8 @@
 package com.study.springboot.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.study.springboot.dto.One2oneDto;
 
 @Mapper
 public interface IOne2oneDao {
 
-	public List<One2oneDto> list();
-	public int write(String one2one_member_id, String one2one_title, String one2one_content);
-	public One2oneDto viewDto(String one2one_idx);
-	public int updateDto(String one2one_idx, String one2one_member_id, String one2one_title, String one2one_content);
-	public int deleteDto(String one2one_idx);
-	
 }
